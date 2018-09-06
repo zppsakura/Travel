1de0b90b2c4df53559ac8892029d816d75d50401
18652953991 <1531047764@qq.com>
2018-09-06 16:38:36 +0800

    add-input

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <city-header></city-header>
+        <city-input></city-input>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CityInput from './components/Input'
 export default {
     name:'City',
     components:{
-        CityHeader
+        CityHeader,
+        CityInput
     } 
 }
 </script>
---
+++ src/pages/city/components/Input.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="input">
+        <input class="city-input" value='' type="text" placeholder="输入城市名或拼音">
+    </div>
+</template>
+
+<script>
+export default {
+    name:'CityInput'
+}
+</script>
+
+<style lang="stylus" scoped>
+    .input
+        height .72rem
+        background #00bcd4
+        padding 0 .1rem
+        .city-input
+            width 100%
+            height .62rem
+            box-sizing border-box
+            line-height .62rem
+            padding 0 .1rem
+            text-align center
+            color #666
+            border-radius .06rem 
+            font-size .30rem
+            
+</style>
+
+
