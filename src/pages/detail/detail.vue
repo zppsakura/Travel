b661da168656ca24703641ec29b70d680a93c1e7
zppsakura <1531047764@qq.com>
2018-09-11 20:04:27 +0800

    finish banner 部分

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,10 +1,16 @@
 <template>
-    
+    <div>
+        <detail-banner></detail-banner>
+    </div>
 </template>
 
 <script>
+import DetailBanner from './components/banner'
 export default {
-    name:'Detail'
+    name:'Detail',
+    components:{
+        DetailBanner
+    }
 }
 </script>
 
