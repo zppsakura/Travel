db74d14525c560ee2c95292ba914d752d0a356a9
zppsakura <1531047764@qq.com>
2018-09-11 19:03:25 +0800

    详情列表页最开始的一小部分

---
+++ src/pages/detail/detail.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name:'Detail'
+}
+</script>
+
+<style lang="stylus" scoped> 
+
+</style>
+
+
--- src/pages/home/components/Commened.vue
+++ src/pages/home/components/Commened.vue
@@ -2,7 +2,13 @@
     <div>
         <div class="title">热销推荐</div>
         <ul>
-            <li class="item" v-for="item of list" :key='item.id'>
+            <router-link 
+            tag="li"
+            class="item" 
+            v-for="item of list" 
+            :key='item.id'
+            :to="'/detail/' + item.id"
+            >
                 <div class="item-img-wrapper">
                     <img class="item-img" :src="item.imgUrl" alt="">
                 </div>
@@ -11,7 +17,7 @@
                     <p class="item-desc">{{item.desc}}</p>
                     <button class="item-btn">查看详情</button>
                 </div>
-            </li>
+            </router-link>
         </ul>
     </div>
 </template>
