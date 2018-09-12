8118bb3ff1e7e468eb85073e1aec35c2d4dd9303
zppsakura <1531047764@qq.com>
2018-09-12 19:09:39 +0800

    递归组件

---
+++ src/pages/detail/components/list.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="list" v-for="(item,index) in list" :key="index">
+            <div class="list-item border-bottom">
+                <span class="list-item-icon"></span>
+                {{item.title}}
+            </div>
+            <div class="list-children" v-if="item.children">
+                <detail-list :list="item.children"></detail-list>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'DetailList',
+    props:{
+        list:Array,
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.list-item
+    line-height .8rem
+    font-size .32rem
+    color #666
+    padding 0 .2rem
+    .list-item-icon
+        display: inline-block;
+        position  relative 
+        left .06rem
+        top .06rem
+        width: .36rem;
+        height: .36rem;
+        background: url(//s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+        margin-right: .1rem;
+        background-size: .4rem 3rem;
+.list-children
+    padding 0 .2rem
+    
+</style>
+
+
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -2,26 +2,63 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
-        <div class="box"></div>
-
+        <div class="box">
+            <detail-list :list="list"></detail-list>
+        </div>
     </div>
 </template>
 
 <script>
-import DetailBanner from './components/banner'
-import DetailHeader from './components/header'
+import DetailBanner from "./components/banner";
+import DetailHeader from "./components/header";
+import DetailList from "./components/list";
 export default {
-    name:'Detail',
-    components:{
-        DetailBanner,
-        DetailHeader
-    }
-}
+  name: "Detail",
+  components: {
+    DetailBanner,
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      list: [
+        {
+          title: "成人票",
+          children: [
+            {
+              title: "成人三馆联票",
+              children:[{
+                  title:'成人三馆联票-某一连锁店销售'
+              },{
+                  title:'成人三馆联票-某二连锁店销售'
+              }]
+            },{
+                title:"成人五馆联票"
+            }
+          ]
+        },
+        {
+          title: "学生票"
+        },
+        {
+          title: "儿童票",
+          children:[{
+              title:"儿童票-1"
+          }]
+        },
+        {
+          title: "特惠票"
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style lang="stylus" scoped> 
-    .box
-        height 50rem
+<style lang="stylus" scoped>
+.box {
+    height: 50rem;
+}
 </style>
 
 
