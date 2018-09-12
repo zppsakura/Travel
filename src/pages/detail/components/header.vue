44d8192c19366b849fd5300f28829310831441c6
zppsakura <1531047764@qq.com>
2018-09-12 16:41:05 +0800

    解绑全局配置（deactivated）

--- src/pages/detail/components/header.vue
+++ src/pages/detail/components/header.vue
@@ -30,7 +30,11 @@ export default {
     },
     activated(){
         window.addEventListener('scroll',this.handleScroll)
+    },
+    deactivated(){
+        window.removeEventListener('scroll',this.handleScroll)
     }
+
 }
 </script>
 
